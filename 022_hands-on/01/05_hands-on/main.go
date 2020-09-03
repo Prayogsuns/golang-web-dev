f30176225ade0498071233ffd7b60c7970c62d44
prayogsuns <prayogsuns@gmail.com>
2020-09-03 19:59:25 +0000

    serverMux exercise second solution 022/01/05

--- 022_hands-on/01/05_hands-on/main.go
+++ 022_hands-on/01/05_hands-on/main.go
@@ -14,39 +14,31 @@ func init() {
 	ghtm = template.Must(template.ParseFiles("ghtm.gohtml"))
 }
 
-type rH string
-type dH string
-type mH string
-
-var rootH rH
-var dogH dH
-var meH mH
-
-func (rh rH) ServeHTTP(w http.ResponseWriter, res *http.Request) {
-	err := ghtm.Execute(w, "Index")
-	if err != nil {
-		log.Println(err)
-	}
-}
-func (dh dH) ServeHTTP(w http.ResponseWriter, res *http.Request) {
-	err := ghtm.Execute(w, "Dog")
-	if err != nil {
-		log.Println(err)
-	}
-}
-func (mh mH) ServeHTTP(w http.ResponseWriter, res *http.Request) {
-	err := ghtm.Execute(w, "Me")
-	if err != nil {
-		log.Println(err)
-	}
-}
 
 func main() {
 
+	rootH := func(w http.ResponseWriter, res *http.Request) {
+		err := ghtm.Execute(w, "Index")
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	dogH := func(w http.ResponseWriter, res *http.Request) {
+		err := ghtm.Execute(w, "Dog")
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	meH := func(w http.ResponseWriter, res *http.Request) {
+		err := ghtm.Execute(w, "Me")
+		if err != nil {
+			log.Println(err)
+		}
+	}
 
-	http.Handle("/", rootH)
-	http.Handle("/dog/", dogH)
-	http.Handle("/me/", meH)
+	http.Handle("/", http.HandlerFunc(rootH))
+	http.Handle("/dog/", http.HandlerFunc(dogH))
+	http.Handle("/me/", http.HandlerFunc(meH))
 
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
