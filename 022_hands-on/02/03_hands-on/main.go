8d7df262dcecdb58f38e7a89e7ac3af684ee7080
prayogsuns <prayogsuns@gmail.com>
2020-09-04 22:11:03 +0000

    serverMux exercise 022/02/03

---
+++ 022_hands-on/02/03_hands-on/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	//"os"
+	"strings"
+)
+
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("Fatal err on Listen: ", err)
+	}
+	defer li.Close()
+
+        for {
+		conn, err := li.Accept()
+		if err != nil {
+			fmt.Println("Connection error on Accept: ", err)
+			continue
+		}
+
+		//io.WriteString(conn, "Hello world!\n")
+		//io.WriteString(conn, fmt.Sprintf("Content-Length: %v\n", len("Hello world!\n")))
+		//conn.Close()
+		//io.WriteString(os.Stdout, "Hello world!\n")
+		go connHandler(conn)
+	}
+}
+
+func connHandler(conn net.Conn) {
+	texts := getRequests(conn)
+	fmt.Println("getRequests: ", texts)
+	writeResponse(conn, texts)
+
+	fmt.Println("Code got here.")
+	io.WriteString(conn, "I see you connected.")
+	conn.Close()
+}
+
+func getRequests(conn net.Conn) string {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line1 := strings.Fields(line)[0]
+		return line1
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Scanner Scan Error", err)
+	}
+	return ""
+
+}
+
+func writeResponse(conn net.Conn, texts string) {
+	fmt.Println("writeResponse: ", texts)
+	_, err := fmt.Fprintln(conn, texts)
+	if err != nil {
+		fmt.Println("writeResponse: ", err)
+	}
+}
