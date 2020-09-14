af08627833f3b3063d2443d8cc0e2ee86699242d
prayogsuns <prayogsuns@gmail.com>
2020-09-14 19:37:39 +0000

    ServeContent exercise 024/07

---
+++ 024_hands-on/07_hands-on/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("starting-files/templates/index.gohtml"))
+}
+
+
+func main() {
+	http.HandleFunc("/", pics)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("starting-files/public"))))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func pics(w http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(w, "")
+	if err != nil {
+		log.Println(err)
+	}
+}
